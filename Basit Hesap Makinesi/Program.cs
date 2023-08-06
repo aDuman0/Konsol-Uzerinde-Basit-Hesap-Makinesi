f455266c592fe44f68e49eb48a38e37aa9b2c8d8
Hunter <Hunter@DESKTOP-3IO628C>
2023-08-06 19:46:08 +0300

    Proje dosyası ekle.

---
+++ Basit Hesap Makinesi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basit_Hesap_Makinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secim, x, y, sonuc;
+
+            Console.WriteLine("Lütfen işlem seçiniz; \n 1-) Toplama \n 2-) Çıkarma \n 3-) Çarpma \n 4-) Bölme");
+            secim = Convert.ToInt32(Console.ReadLine());
+
+            switch (secim)
+            {
+
+                case 1:
+                    Console.WriteLine("İlk sayınızı giriniz:");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("İkinci sayınızı giriniz:");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    sonuc = x + y;
+                    Console.WriteLine(sonuc);
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.WriteLine("İlk sayınızı giriniz:");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("İkinci sayınızı giriniz:");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    sonuc = x - y;
+                    Console.WriteLine(sonuc);
+                    Console.ReadLine();
+                    break;
+
+                case 3:
+                    Console.WriteLine("İlk sayınızı giriniz:");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("İkinci sayınızı giriniz:");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    sonuc = x * y;
+                    Console.WriteLine(sonuc);
+                    Console.ReadLine();
+                    break;
+
+                case 4:
+                    Console.WriteLine("İlk sayınızı giriniz:");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("İkinci sayınızı giriniz:");
+                    y = Convert.ToInt32(Console.ReadLine());
+                    sonuc = x / y;
+                    Console.WriteLine(sonuc);
+                    Console.ReadLine();
+                    break;
+
+            }
+        }
+    }
+}
